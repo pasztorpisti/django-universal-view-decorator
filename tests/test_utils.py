3b2b3233b5ef23aa603da1bf4d65a92b9787c83c
Istvan Pasztor <pasztorpisti@gmail.com>
2016-04-04 19:53:17 +0100

    adding a test for class_property

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,3 +13,14 @@ class TestClassProperty(TestCase):
 
     def test_with_instance(self):
         self.assertEqual(self.MyClass().my_class_property, (self.MyClass, 'class_property'))
+
+    def test_using_getter(self):
+        class MyClass(object):
+            my_class_property = class_property()
+
+            @my_class_property.getter
+            def could_be_my_class_property_but_it_isnt_because_of_testing(cls):
+                return cls, 'class_property'
+
+        self.assertEqual(MyClass.my_class_property, (MyClass, 'class_property'))
+        self.assertEqual(MyClass.could_be_my_class_property_but_it_isnt_because_of_testing, (MyClass, 'class_property'))
