e094643d5b470dcc13b45f8a79f7ab724e4a76be
Istvan Pasztor <pasztorpisti@gmail.com>
2016-04-05 13:11:01 +0100

    adding LOGGING config

--- tests/settings.py
+++ tests/settings.py
@@ -11,3 +11,25 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '[%(name)s][%(levelname)s] %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+        },
+    },
+}
