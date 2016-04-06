f39f3c57433bca96fbd5495538e252dad60c569d
Istvan Pasztor <pasztorpisti@gmail.com>
2016-04-06 10:31:44 +0100

    A real-life-ish test with a django test application.
    These tests issue requests to the decorated views through the django test client (our test requests go through the django stack) unlike the rest of our lightweight (but performant) view testing that doesn't use the django request handler stack and communicates directly with the tested view.

--- tests/settings.py
+++ tests/settings.py
@@ -1,10 +1,8 @@
-import os
-
 DEBUG = True
 
 SECRET_KEY = 'TOP_SECRET'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+ROOT_URLCONF = 'tests.test_app.urls'
 
 DATABASES = {
     'default': {
