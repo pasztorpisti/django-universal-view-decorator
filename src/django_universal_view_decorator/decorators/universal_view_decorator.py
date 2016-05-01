d597acfeeae6e6eb6ef34368977150329bafa548
Istvan Pasztor <pasztorpisti@gmail.com>
2016-05-01 16:21:14 +0100

    duplicate handler params for universal_view_decorator
    Added the duplicate_id, duplicate_priority, duplicate_keep_newest and duplicate_handler_func keyword only parameters for universal_view_decorator and universal_view_decorator_with_args.

--- src/django_universal_view_decorator/decorators/universal_view_decorator.py
+++ src/django_universal_view_decorator/decorators/universal_view_decorator.py
@@ -1,10 +1,13 @@
 import inspect
 
-from .view_routine_decorator import view_routine_decorator
 from .view_class_decorator import view_class_decorator
+from .view_routine_decorator import view_routine_decorator
+from ..utils import wraps
 
 
-def universal_view_decorator(*decorators):
+def universal_view_decorator(*decorators, **duplicate_params):
+    decorators = _wrap_decorators_if_needed(decorators, **duplicate_params)
+
     def decorate(view):
         if not decorators:
             return view
@@ -13,12 +16,40 @@ def universal_view_decorator(*decorators):
     return decorate
 
 
-def universal_view_decorator_with_args(decorator):
+def universal_view_decorator_with_args(decorator, **duplicate_params):
     def receive_decorator_args(*args, **kwargs):
         parametrized_decorator = decorator(*args, **kwargs)
+        parametrized_decorator = _wrap_decorators_if_needed((parametrized_decorator,), **duplicate_params)[0]
 
         def decorate(view):
             decorator_wrapper = view_routine_decorator if inspect.isroutine(view) else view_class_decorator
             return decorator_wrapper(parametrized_decorator)(view)
         return decorate
     return receive_decorator_args
+
+
+def _wrap_decorators_if_needed(decorators, duplicate_id=None, duplicate_handler_func=None,
+                               duplicate_keep_newest=None, duplicate_priority=None):
+    if duplicate_id is None and duplicate_handler_func is None and \
+            duplicate_keep_newest is None and duplicate_priority is None:
+        return decorators
+    if duplicate_id is None:
+        raise ValueError("You have used duplicate decorator related parameters without the 'duplicate_id' parameter")
+    attributes = {}
+    if duplicate_id is not None:
+        attributes['decorator_duplicate_id'] = duplicate_id
+    if duplicate_handler_func is not None:
+        attributes['decorator_duplicate_handler_func'] = duplicate_handler_func
+    if duplicate_keep_newest is not None:
+        attributes['decorator_duplicate_keep_newest'] = duplicate_keep_newest
+    if duplicate_priority is not None:
+        attributes['decorator_duplicate_priority'] = duplicate_priority
+    return tuple(_wrap_decorator_with_attributes(decorator, attributes) for decorator in decorators)
+
+
+def _wrap_decorator_with_attributes(decorator, attributes):
+    @wraps(decorator)
+    def wrapper(*args, **kwargs):
+        return decorator(*args, **kwargs)
+    wrapper.__dict__.update(attributes)
+    return wrapper
--- tests/test_universal_view_decorator.py
+++ tests/test_universal_view_decorator.py
@@ -1,8 +1,9 @@
 import functools
+import re
+
 import mock
 from django.test import TestCase
 from django.views.generic import View
-
 from django_universal_view_decorator import universal_view_decorator, universal_view_decorator_with_args
 
 
@@ -195,3 +196,165 @@ class TestDecorationWithStackedDecorators(TestCase):
             mock.call('decorator', 4),
             mock.call('dispatch', ViewClass),
         ])
+
+
+@mock.patch(__name__ + '.test_log', wraps=test_log)
+class TestDecoratorDuplicateHandlingParamsWork(TestCase):
+    def test_lonely_duplicate_id_doesnt_change_anything(self, mock_test_log):
+        wrapped_decorator = universal_view_decorator_with_args(decorator)
+
+        @universal_view_decorator(decorator(1), duplicate_id='id')
+        @universal_view_decorator(decorator(2))
+        @wrapped_decorator(3)
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 1),
+            mock.call('decorator', 2),
+            mock.call('decorator', 3),
+            mock.call('dispatch', ViewClass),
+        ])
+
+    def test_duplicate_id_keeps_oldest(self, mock_test_log):
+        wrapped_decorator = universal_view_decorator_with_args(decorator, duplicate_id='id')
+
+        @universal_view_decorator(decorator(1), duplicate_id='id')
+        @universal_view_decorator(decorator(2))
+        @wrapped_decorator(3)
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 2),
+            mock.call('decorator', 3),
+            mock.call('dispatch', ViewClass),
+        ])
+
+    def test_duplicate_id_keeps_newest(self, mock_test_log):
+        wrapped_view_decorator = universal_view_decorator_with_args(decorator, duplicate_id='id')
+
+        @universal_view_decorator(decorator(1), duplicate_id='id', duplicate_keep_newest=True)
+        @universal_view_decorator(decorator(2))
+        @wrapped_view_decorator(3)
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 1),
+            mock.call('decorator', 2),
+            mock.call('dispatch', ViewClass),
+        ])
+
+    def test_duplicate_priority(self, mock_test_log):
+        wrapped_view_decorator = universal_view_decorator_with_args(decorator, duplicate_id='id')
+
+        @universal_view_decorator(decorator(1), duplicate_id='id', duplicate_priority=1)
+        @universal_view_decorator(decorator(2))
+        @wrapped_view_decorator(3)  # default duplicate_priority=0
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 1),
+            mock.call('decorator', 2),
+            mock.call('dispatch', ViewClass),
+        ])
+
+        mock_test_log.reset_mock()
+
+        @universal_view_decorator(decorator(1), duplicate_id='id', duplicate_priority=-1)
+        @universal_view_decorator(decorator(2))
+        @wrapped_view_decorator(3)  # default duplicate_priority=0
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 2),
+            mock.call('decorator', 3),
+            mock.call('dispatch', ViewClass),
+        ])
+
+    def test_duplicate_handler_func(self, mock_test_log):
+        def keep_newest(duplicate_id, duplicates):
+            for i in range(1, len(duplicates)):
+                duplicates[i] = None
+
+        def keep_oldest(duplicate_id, duplicates):
+            for i in range(len(duplicates)-1):
+                duplicates[i] = None
+
+        wrapped_view_decorator = universal_view_decorator_with_args(decorator, duplicate_id='id')
+
+        @universal_view_decorator(decorator(1), duplicate_id='id', duplicate_handler_func=keep_newest)
+        @universal_view_decorator(decorator(2))
+        @wrapped_view_decorator(3)
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 1),
+            mock.call('decorator', 2),
+            mock.call('dispatch', ViewClass),
+        ])
+
+        mock_test_log.reset_mock()
+
+        @universal_view_decorator(decorator(1), duplicate_id='id', duplicate_handler_func=keep_oldest)
+        @universal_view_decorator(decorator(2))
+        @wrapped_view_decorator(3)
+        class ViewClass(View):
+            def dispatch(self, request, *args, **kwargs):
+                test_log('dispatch', ViewClass)
+                return 'response'
+
+        response = ViewClass.as_view()('request')
+        self.assertEqual(response, 'response')
+        self.assertListEqual(mock_test_log.mock_calls, [
+            mock.call('decorator', 2),
+            mock.call('decorator', 3),
+            mock.call('dispatch', ViewClass),
+        ])
+
+
+class TestDuplicateIdParamHasToBeSpecified(TestCase):
+    """ If a parameter related to duplicate handling is present then the duplicate_id parameter has to be present.
+    The rest of the duplicate decorator related parameters are meaningless without duplicate_id. """
+    def test_duplicate_priority_without_duplicate_id_fails(self):
+        with self.assertRaisesRegexp(ValueError, re.escape(
+                r"You have used duplicate decorator related parameters without the 'duplicate_id' parameter")):
+            universal_view_decorator(decorator(0), duplicate_priority=0)
+
+    def test_duplicate_keep_newest_without_duplicate_id_fails(self):
+        with self.assertRaisesRegexp(ValueError, re.escape(
+                r"You have used duplicate decorator related parameters without the 'duplicate_id' parameter")):
+            universal_view_decorator(decorator(0), duplicate_keep_newest=False)
+
+    def test_duplicate_handler_func_without_duplicate_id_fails(self):
+        with self.assertRaisesRegexp(ValueError, re.escape(
+                r"You have used duplicate decorator related parameters without the 'duplicate_id' parameter")):
+            universal_view_decorator(decorator(0), duplicate_handler_func=lambda duplicate_id, duplicates: None)
